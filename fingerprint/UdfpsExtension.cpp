5689aa85d0f62ab44e108c0d6c1caa04b6a159a6
pjgowtham <pjgowtham@gmail.com>
2024-09-29 01:36:20 +0530

    tundra: fingerprint: Set dim layer zOrder
    Requires: I367b2c395aeb8cb7cb9e3cc38eca98477befe007
    Change-Id: I881db78186fa9c7b350e79ac4ef90f53ee4cfbcf
    (cherry picked from commit 2e3a0cda79ef7a1cf9dff430cfaa83cea1b46551)

--- fingerprint/UdfpsExtension.cpp
+++ fingerprint/UdfpsExtension.cpp
@@ -2,10 +2,13 @@
  * SPDX-FileCopyrightText: The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
- 
 
 #include <compositionengine/UdfpsExtension.h>
 #include <display/drm/sde_drm.h>
+
+uint32_t getUdfpsDimZOrder(uint32_t z) {
+    return z;
+}
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
     if (touched) {
