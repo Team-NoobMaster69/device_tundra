e6167e30baa86dba631bb1aa7c6cf3150b97c7fe
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    tundra: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- device.mk
+++ device.mk
@@ -412,7 +412,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors-service.multihal \
     sensors.tundra \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     android.hardware.sensors@1.0-service \
