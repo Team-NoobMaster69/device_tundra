e98374b298d2128601eb40202fe9947eb7841105
Michael Bestas <mkbestas@lineageos.org>
2025-03-11 02:17:16 +0200

    tundra: Add hardware/motorola to soong namespaces
    Change-Id: I1606d2dc9cef82a4ce9d8764c1f52b59d183e9b2

--- device.mk
+++ device.mk
@@ -433,7 +433,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/motorola
 
 # Telephony
 PRODUCT_PACKAGES += \
