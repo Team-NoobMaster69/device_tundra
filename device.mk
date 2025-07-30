30bfbb02bb5638690ac2823bc540fcf83a27cd5a
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    tundra: Disable `thumbnail_block_model` of libstagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

--- device.mk
+++ device.mk
@@ -117,6 +117,8 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libcamera2ndk_vendor
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
