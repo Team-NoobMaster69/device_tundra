cfc51947b9c09a1f77dbfc7593430d6017da1bd9
HotaruOs <arroztlg@gmail.com>
2025-12-15 09:02:36 -0300

    logs

--- device.mk
+++ device.mk
@@ -104,6 +104,15 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+# Logs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1 \
+    persist.sys.usb.config=adb \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
